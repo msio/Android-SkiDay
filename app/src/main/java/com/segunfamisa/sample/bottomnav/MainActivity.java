bbac1681db4b66dcddbc408391383254f2cb1b14
patrick5258 <patrickwolfschwenger@yahoo.de>
2017-04-25 15:35:08 +0200

    initial

---
+++ app/src/main/java/com/segunfamisa/sample/bottomnav/MainActivity.java
@@ -0,0 +1,124 @@
+package com.segunfamisa.sample.bottomnav;
+
+import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+
+        TabHost host = (TabHost)findViewById(R.id.tabhost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Meine Events");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Veranstalter");
+        host.addTab(spec);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        MenuItem homeItem = mBottomNav.getMenu().getItem(0);
+        if (mSelectedItem != homeItem.getItemId()) {
+            // select home item
+            selectFragment(homeItem);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        /*
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.menu_timeline:
+                frag = MenuFragment.newInstance(getString(R.string.text_timeline),
+                        getColorFromRes(R.color.color_timeline));
+                break;
+            case R.id.menu_ranking:
+                frag = MenuFragment.newInstance(getString(R.string.text_ranking),
+                        getColorFromRes(R.color.color_ranking));
+                break;
+            case R.id.menu_social:
+                frag = MenuFragment.newInstance(getString(R.string.text_social),
+                        getColorFromRes(R.color.color_social));
+                break;
+        }   */
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i = 0; i< mBottomNav.getMenu().size(); i++) {
+            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        updateToolbarText(item.getTitle());
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+
+    private void updateToolbarText(CharSequence text) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(text);
+        }
+    }
+
+    private int getColorFromRes(@ColorRes int resId) {
+        return ContextCompat.getColor(this, resId);
+    }
+}
