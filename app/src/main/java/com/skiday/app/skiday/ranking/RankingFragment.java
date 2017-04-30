05cd63d99ad04c92251c06be14b688ec9fc81ed4
Jan Amort <e0425530@student.tuwien.ac.at>
2017-04-30 18:11:23 +0200

    Ranking tabs added

--- app/src/main/java/com/skiday/app/skiday/ranking/RankingFragment.java
+++ app/src/main/java/com/skiday/app/skiday/ranking/RankingFragment.java
@@ -1,11 +1,12 @@
 package com.skiday.app.skiday.ranking;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 import com.skiday.app.skiday.R;
 import com.skiday.app.skiday.social.SocialFragment;
@@ -16,8 +17,8 @@ import com.skiday.app.skiday.social.SocialFragment;
 
 public class RankingFragment extends Fragment {
 
-    public static SocialFragment newInstance() {
-        SocialFragment fragment = new SocialFragment();
+    public static RankingFragment newInstance() {
+        RankingFragment fragment = new RankingFragment();
         return fragment;
     }
 
@@ -35,5 +36,30 @@ public class RankingFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TabHost host = (TabHost) view.findViewById(R.id.tabHostRanking);
+        host.setup();
+        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+
+            }
+        });
+
+        TabHost.TabSpec spec = host.newTabSpec("Durchgang 1");
+        spec.setContent(R.id.tab1Ranking);
+        spec.setIndicator("Durchgang 1");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Durchgang 2");
+        spec.setContent(R.id.tab2Ranking);
+        spec.setIndicator("Durchgang 2");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Gesamt");
+        spec.setContent(R.id.tab3Ranking);
+        spec.setIndicator("Gesamt");
+        host.addTab(spec);
+
     }
 }
