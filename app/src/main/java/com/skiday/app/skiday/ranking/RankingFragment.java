8bc25cef05e3fad37bebeb68ec59b0b04c61fe42
Jan Amort <e0425530@student.tuwien.ac.at>
2017-05-02 20:12:21 +0200

    Ranking tables added

--- app/src/main/java/com/skiday/app/skiday/ranking/RankingFragment.java
+++ app/src/main/java/com/skiday/app/skiday/ranking/RankingFragment.java
@@ -3,10 +3,14 @@ package com.skiday.app.skiday.ranking;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.skiday.app.skiday.R;
 import com.skiday.app.skiday.social.SocialFragment;
@@ -16,6 +20,12 @@ import com.skiday.app.skiday.social.SocialFragment;
  */
 
 public class RankingFragment extends Fragment {
+
+    private String [][] rankData ={
+            {"1.", "Marcel Hirscher", "1:19:85", "0", "0.3"},
+            {"2.", "H. Kristoffersen", "1:20:10", "+0.1", "-0.2"},
+            {"3.", "Hermann Maier", "1:22:85", "+0.3", "0"},
+    };
 
     public static RankingFragment newInstance() {
         RankingFragment fragment = new RankingFragment();
@@ -46,14 +56,14 @@ public class RankingFragment extends Fragment {
             }
         });
 
-        TabHost.TabSpec spec = host.newTabSpec("Durchgang 1");
+        TabHost.TabSpec spec = host.newTabSpec("Lap 1");
         spec.setContent(R.id.tab1Ranking);
-        spec.setIndicator("Durchgang 1");
+        spec.setIndicator("Lap 1");
         host.addTab(spec);
 
-        spec = host.newTabSpec("Durchgang 2");
+        spec = host.newTabSpec("Lap 2");
         spec.setContent(R.id.tab2Ranking);
-        spec.setIndicator("Durchgang 2");
+        spec.setIndicator("Lap 2");
         host.addTab(spec);
 
         spec = host.newTabSpec("Gesamt");
@@ -61,5 +71,74 @@ public class RankingFragment extends Fragment {
         spec.setIndicator("Gesamt");
         host.addTab(spec);
 
+        setupTable(view);
+
+    }
+
+    public void setupTable(View view){
+
+        TableLayout table = (TableLayout) view.findViewById(R.id.ranking_table_lap1);
+        TableLayout table1 = (TableLayout) view.findViewById(R.id.ranking_table_lap2);
+        TableLayout table2 = (TableLayout) view.findViewById(R.id.ranking_table_result);
+
+        for (int i = 0; i< rankData.length; i++){
+
+            table.addView(getRow(i));
+            table1.addView(getRow(i));
+            table2.addView(getRow(i));
+        }
+
+    }
+
+    private TableRow getRow(int index){
+
+        int textColor = getResources().getColor(R.color.colorRankTableFieldText);
+
+        TableRow row = new TableRow(getContext());
+
+        // Rank
+        TextView field = new TextView(getContext());
+        field.setText(rankData[index][0]);
+        field.setTextSize(13);
+        field.setTextColor(textColor);
+        field.setHeight(90);
+        row.addView(field);
+
+        // Name
+        field = new TextView(getContext());
+        field.setText(rankData[index][1]);
+        field.setTextSize(13);
+        field.setHeight(90);
+        field.setTextColor(textColor);
+
+        row.addView(field);
+
+        // Time
+        field = new TextView(getContext());
+        field.setText(rankData[index][2]);
+        field.setTextSize(13);
+        field.setTextColor(textColor);
+        field.setHeight(90);
+        row.addView(field);
+
+        // Time in relation to best
+        field = new TextView(getContext());
+        field.setText(rankData[index][3]);
+        field.setTextSize(13);
+        field.setTextColor(textColor);
+        field.setWidth(90);
+        field.setGravity(Gravity.END);
+        row.addView(field);
+
+        // Time in relation to "me"
+        field = new TextView(getContext());
+        field.setText(rankData[index][4]);
+        field.setTextSize(13);
+        field.setTextColor(textColor);
+        field.setWidth(50);
+        field.setGravity(Gravity.END);
+        row.addView(field);
+
+        return row;
     }
 }
