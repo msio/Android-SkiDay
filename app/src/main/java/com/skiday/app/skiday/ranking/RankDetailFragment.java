f91947625215c99e92a17b2ebb00aea180238dbf
Jan Amort <e0425530@student.tuwien.ac.at>
2017-05-03 20:12:16 +0200

    Ranking refined

--- app/src/main/java/com/skiday/app/skiday/ranking/RankDetailFragment.java
+++ app/src/main/java/com/skiday/app/skiday/ranking/RankDetailFragment.java
@@ -1,5 +1,6 @@
 package com.skiday.app.skiday.ranking;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +9,14 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.skiday.app.skiday.R;
+import com.skiday.app.skiday.model.PersonResult;
+import com.skiday.app.skiday.model.Result;
 
 /**
  * Created by jan on 03.05.17.
@@ -26,15 +30,23 @@ public class RankDetailFragment extends Fragment {
             {"00:15.23", "+1.2", "-2.1"},
             {"00:35.10", "+0.5", "-1.2"},
             {"01:01,12", "+0,1", "-1.3"},
+            {"01:19,85", "0", "+1.3"},
     };
 
-    private String result = "01:19,85";
+    private int id;
+    PersonResult personResult;
 
-    public static RankDetailFragment newInstance() {
+    public static RankDetailFragment newInstance(int id) {
         Log.i(TAG, "newInstance: ");
         RankDetailFragment fragment = new RankDetailFragment();
+        fragment.setId(id);
         return fragment;
     }
+
+    public void setId(int id){
+        this.id = id;
+    }
+        //personResult = RankingFragment.getData().get(index);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +57,10 @@ public class RankDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.i(TAG, "onCreateView: ");
+        personResult = RankingFragment.getData().get(id-1);
+
+
+
         return inflater.inflate(R.layout.fragment_rank_detail, container, false);
     }
 
@@ -53,17 +69,27 @@ public class RankDetailFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.i(TAG, "onViewCreated: ");
 
+        TextView field = (TextView) view.findViewById (R.id.name);
+        field.setText(personResult.getName());
+
+        field = (TextView) view.findViewById (R.id.current_time);
+        field.setText(personResult.getTotal().getTime());
+
+        ImageView profile = (ImageView) view.findViewById (R.id.profile_image);
+        Drawable drawable = getResources().getDrawable(personResult.getPictureId());
+        profile.setImageDrawable(drawable);
+
         TableLayout table = (TableLayout) view.findViewById(R.id.intermed_results);
 
-        table.addView(getRow(0));
-        table.addView(getRow(1));
-        table.addView(getRow(2));
-        table.addView(getResultRow());
+        table.addView(getRow("1. Meantime", personResult.getLap1().getMeantime1()));
+        table.addView(getRow("2. Meantime", personResult.getLap1().getMeantime2()));
+        table.addView(getRow("3. Meantime", personResult.getLap1().getMeantime3()));
+        table.addView(getResultRow("Total", personResult.getLap1().getTotal()));
 
     }
 
 
-    private TableRow getRow(int index){
+    private TableRow getRow(String caption, Result result){
 
         int textColor = getResources().getColor(R.color.colorRankTableFieldText);
         int headerColor = getResources().getColor(R.color.colorRankTableHeader);
@@ -72,7 +98,7 @@ public class RankDetailFragment extends Fragment {
 
         // Header
         TextView field = new TextView(getContext());
-        field.setText((index+1)+". Meantime");
+        field.setText(caption);
         field.setTextSize(13);
         field.setTextColor(headerColor);
         field.setHeight(90);
@@ -81,7 +107,7 @@ public class RankDetailFragment extends Fragment {
 
         // Time
         field = new TextView(getContext());
-        field.setText(resultTable[index][0]);
+        field.setText(result.getTime());
         field.setTextSize(13);
         field.setHeight(90);
         field.setWidth(200);
@@ -91,7 +117,7 @@ public class RankDetailFragment extends Fragment {
 
         // Time in relation to best
         field = new TextView(getContext());
-        field.setText(resultTable[index][1]);
+        field.setText(result.getRelativeToBest());
         field.setTextSize(13);
         field.setTextColor(textColor);
         field.setWidth(150);
@@ -100,7 +126,7 @@ public class RankDetailFragment extends Fragment {
 
         // Time in relation to "me"
         field = new TextView(getContext());
-        field.setText(resultTable[index][2]);
+        field.setText(result.getRelativeToMe());
         field.setTextSize(13);
         field.setTextColor(textColor);
         field.setWidth(150);
@@ -111,30 +137,52 @@ public class RankDetailFragment extends Fragment {
     }
 
 
-    private TableRow getResultRow(){
+    private TableRow getResultRow(String caption, Result result){
 
         int textColor = getResources().getColor(R.color.colorRankTableFieldText);
         int headerColor = getResources().getColor(R.color.colorRankTableHeader);
+        int primaryColor = getResources().getColor(R.color.colorPrimary);
 
         TableRow row = new TableRow(getContext());
 
         // Header
         TextView field = new TextView(getContext());
-        field.setText("Total:");
+        field.setText(caption);
         field.setTextSize(13);
         field.setTextColor(headerColor);
         field.setHeight(90);
+        field.setWidth(350);
         row.addView(field);
 
         // Time
         field = new TextView(getContext());
-        field.setText(result);
+        field.setText(result.getTime());
         field.setTextSize(13);
         field.setHeight(90);
-        field.setTextColor(textColor);
+        field.setWidth(200);
+        field.setTextColor(primaryColor);
 
         row.addView(field);
 
+        // Time in relation to best
+        field = new TextView(getContext());
+        field.setText(result.getRelativeToBest());
+        field.setTextSize(13);
+        field.setTextColor(primaryColor);
+        field.setWidth(150);
+        field.setGravity(Gravity.END);
+        row.addView(field);
+
+        // Time in relation to "me"
+        field = new TextView(getContext());
+        field.setText(result.getRelativeToMe());
+        field.setTextSize(13);
+        field.setTextColor(primaryColor);
+        field.setWidth(150);
+        field.setGravity(Gravity.END);
+        row.addView(field);
+
         return row;
+
     }
 }
