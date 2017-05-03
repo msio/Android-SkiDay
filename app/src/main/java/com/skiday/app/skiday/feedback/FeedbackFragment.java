f7a76f66c6ee4a19c6c546ba37d671db7f7b25e2
Michal Vranec <vranec.michal@gmail.com>
2017-05-03 20:31:11 +0200

    update

---
+++ app/src/main/java/com/skiday/app/skiday/feedback/FeedbackFragment.java
@@ -0,0 +1,39 @@
+package com.skiday.app.skiday.feedback;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.skiday.app.skiday.R;
+import com.skiday.app.skiday.social.SocialFragment;
+
+public class FeedbackFragment extends Fragment{
+    public static FeedbackFragment newInstance(){
+        FeedbackFragment fragment = new FeedbackFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_social_feedback, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void test(View view){
+
+    }
+}
