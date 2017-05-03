9552012278ef6bf2ee82c5729f32751989433aa7
Michal Vranec <vranec.michal@gmail.com>
2017-05-03 17:07:50 +0200

    event details

--- app/src/main/java/com/skiday/app/skiday/timeline/EventDetails.java
+++ app/src/main/java/com/skiday/app/skiday/timeline/EventDetails.java
@@ -4,6 +4,9 @@ package com.skiday.app.skiday.timeline;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,26 +14,23 @@ import android.view.Window;
 
 import com.skiday.app.skiday.R;
 
+
 /**
  * Created by msio on 5/2/17.
  */
 
-public class EventDetails extends DialogFragment {
+public class EventDetails extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dialog_event_details, container, false);
+//        Bundle args = getArguments();
+//        String title = args.getString("title");
+        View v = inflater.inflate(R.layout.dialog_event_details, container, false);
+//        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+//        toolbar.setTitle("TEST");
+        return v;
     }
 
-    /*@Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // The only reason you might override this method when using onCreateView() is
-        // to modify any dialog characteristics. For example, the dialog includes a
-        // title by default, but your custom layout might not need it. So here you can
-        // remove the dialog title, but you must call the superclass to get the Dialog.
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        return dialog;
-    }*/
 }
